156c097d61215933bb63deeb6c78ecdfc6bb8035
Marijn Haverbeke <marijn@haverbeke.nl>
2019-11-19 11:02:37 +0100

    Output a bundled, Bublé-ed mjs file in dist/
    FIX: The file referred to in the package's `module` field now is
    compiled down to ES5.
    
    Issue prosemirror/prosemirror#1003

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   input: "./src/index.js",
-  output: {format: "cjs", file: "dist/index.js"},
+  output: [{
+    file: 'dist/index.js',
+    format: 'cjs',
+    sourcemap: true
+  }, {
+    file: 'dist/index.mjs',
+    format: 'es',
+    sourcemap: true
+  }],
   sourcemap: true,
-  plugins: [require("rollup-plugin-buble")()],
+  plugins: [require('@rollup/plugin-buble')()],
   external(id) { return !/^[\.\/]/.test(id) }
 }
