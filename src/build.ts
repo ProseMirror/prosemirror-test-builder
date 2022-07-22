49fe7b9c99722381875b66607f1bac60537e3ffb
Hirotaka Miyagi <31152321+MH4GF@users.noreply.github.com>
2022-07-22 16:02:29 +0900

    Refine return type of builders
    Until now, the only explicit return type of builder has been schema.
    Therefore, to use the builder in a TypeScript project, it had to be
    cast to any.
    
    This change provides a type that uses the schema and name of the
    builders argument to return the appropriate object.
    
    FIX: Make the return type of \`builders\` include properties for
    schema node/mark names.

--- src/build.ts
+++ src/build.ts
@@ -68,6 +68,16 @@ function takeAttrs(attrs: Attrs | null, args: [a?: Attrs | ChildSpec, ...b: Chil
 export type NodeBuilder = (attrsOrFirstChild?: Attrs | ChildSpec, ...children: ChildSpec[]) => Node
 export type MarkBuilder = (attrsOrFirstChild?: Attrs | ChildSpec, ...children: ChildSpec[]) => ChildSpec
 
+type Builders<S extends Schema> = {
+  schema: S;
+} & {
+  [key in keyof S['nodes']]: NodeBuilder
+} & {
+  [key in keyof S['marks']]: MarkBuilder
+} & {
+  [name: string]: NodeBuilder | MarkBuilder
+}
+
 /// Create a builder function for nodes with content.
 function block(type: NodeType, attrs: Attrs | null = null): NodeBuilder {
   let result: NodeBuilder = function(...args) {
@@ -93,7 +103,7 @@ function mark(type: MarkType, attrs: Attrs | null): MarkBuilder {
   }
 }
 
-export function builders(schema: Schema, names?: {[name: string]: Attrs}) {
+export function builders<Nodes extends string = any, Marks extends string = any>(schema: Schema<Nodes, Marks>, names?: {[name: string]: Attrs}) {
   let result = {schema}
   for (let name in schema.nodes) (result as any)[name] = block(schema.nodes[name], {})
   for (let name in schema.marks) (result as any)[name] = mark(schema.marks[name], {})
@@ -103,5 +113,5 @@ export function builders(schema: Schema, names?: {[name: string]: Attrs}) {
     if (type = schema.nodes[typeName]) (result as any)[name] = block(type, value)
     else if (type = schema.marks[typeName]) (result as any)[name] = mark(type, value)
   }
-  return result
+  return result as Builders<Schema<Nodes, Marks>>
 }
